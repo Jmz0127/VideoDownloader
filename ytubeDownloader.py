8a887ff019833d0043f21ae11d49a9b74cb188e3
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2023-09-09 22:19:49 -0400

    added folder download path and resolution fix

--- ytubeDownloader.py
+++ ytubeDownloader.py
@@ -1,11 +1,15 @@
 from pytube import YouTube
 from sys import argv
-# argv takes all the things you input into command line in this program
 
 link = argv[1]
-# do not use index 0 as 0 is reserved for the program name
 yt = YouTube(link)
 
-print("The Title of the video: ", yt.title)
+print("Title: ", yt.title)
 
-print("Views: ", yt.views)
+print("View: ", yt.views)
+
+# formats to highest res allowed
+yd = yt.streams.get_highest_resolution()
+
+# add desired folder below
+yd.download('./YTfolder')
